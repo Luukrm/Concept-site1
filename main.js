a12f55d1208630dd0eacd6354200e05ee16e3caa
Luuk M <lm2k@protonmail.com>
2021-01-28 17:15:50 +0100

    finished first concept site

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let i;
 
 async function countingThroughWeight() {
-    for (i = 0; i < 86; i++) {
-        document.getElementById("weight").innerHTML = i + 'kg';
+    for (let i = 0; i < 99; i++) {
+        document.getElementById("weight").innerHTML = i + 'Kg';
         await sleep(25);
     }
 }
 
 async function countingThroughFat() {
-    for (i = 0; i < 18; i++) {
+    for (let i = 0; i < 99; i++) {
         document.getElementById("fat").innerHTML = i + '%';
         await sleep(35);
     }
 }
+
 async function countingThroughMuscle() {
-    for (i = 0; i < 44; i++) {
-        document.getElementById("muscle").innerHTML = i +1 + 'kg';
+    for (let i = 0; i < 99; i++) {
+        document.getElementById("muscle").innerHTML = i + 'Kg';
         await sleep(25);
     }
 }
@@ -25,12 +25,25 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+window.onscroll = function () {
+    myFunction()
+};
 
-countingThroughWeight();
-countingThroughFat();
-countingThroughMuscle();
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+        console.log("yeetus");
+    }
+}
+
+
 $(document).scroll(function () {
-    var y = $(this).scrollTop();
+    let y = $(this).scrollTop();
     if (y > 50) {
         $('.stats-number').fadeIn();
     } else {
@@ -38,3 +51,6 @@ $(document).scroll(function () {
     }
 
 });
+countingThroughWeight();
+countingThroughFat();
+countingThroughMuscle();
