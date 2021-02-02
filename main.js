d4c15702c8f10792fb657946fe15e37b76a9b9b7
Luuk M <lm2k@protonmail.com>
2021-02-02 15:24:17 +0100

    finished bmi calcualtor

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 async function countingThroughWeight() {
     for (let i = 0; i < 99; i++) {
         document.getElementById("weight").innerHTML = i + 'Kg';
@@ -51,6 +50,39 @@ $(document).scroll(function () {
     }
 });
 
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
+function calcBMI() {
+    let height = document.getElementById("inputHeight").value;
+    let weight = document.getElementById("inputWeight").value;
+    let result = roundToTwo(weight / (height * height) * 10000);
+
+    if (result < 18.5) {
+        document.getElementById('category').innerHTML = "You are underweight";
+        document.getElementById('header').style.backgroundColor = '#ff1f0a';
+        document.getElementById('title').style.color = '#ffffff';
+
+    } else if (result > 18.5 && result < 25) {
+        document.getElementById('category').innerHTML = "You have a healthy weight";
+        document.getElementById('header').style.backgroundColor = '#11ff00';
+        document.getElementById('title').style.color = '#ffffff';
+
+    } else if (result > 25 && result < 30) {
+        document.getElementById('category').innerHTML = "You are overweight";
+        document.getElementById('header').style.backgroundColor = '#ff9100';
+        document.getElementById('title').style.color = '#ffffff';
+
+    } else {
+        document.getElementById('category').innerHTML = "You are obese";
+        document.getElementById('header').style.backgroundColor = '#ff1f0a';
+        document.getElementById('title').style.color = '#ffffff';
+
+    }
+    document.getElementById('result').innerHTML = "Result: " + result;
+
+}
 
 countingThroughWeight();
 countingThroughFat();
